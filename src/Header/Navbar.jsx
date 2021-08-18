e53791d59a30fb1e59d54f3f35e0246fd4d2b334
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-08-18 19:04:07 +0400

    feat: changed classnames to camelCase and import space added

--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import logo from "../assets/images/logo.jpg";
 import menuIcon from "../assets/images/menu-icon.svg";
 import closeIcon from "../assets/images/close_burger.svg";
@@ -11,7 +12,7 @@ export default function Navbar() {
 
   return (
     <>
-      <div className={styles.MAINNAVBAR}>
+      <div className={styles.mainNavbar}>
         <div>
           <img className={styles.LOGO} src={logo} />
         </div>
@@ -24,24 +25,24 @@ export default function Navbar() {
           <img hidden={mobileMenuIsHidden} src={closeIcon} />
         </div>
 
-        <div className={styles.LOGGERDEPARTMENTS}>
-          <div className={styles.DEPARTMENTS}>
-            <h4 className={styles.DEPARTMENT}>BOOKS</h4>
-            <h4 className={styles.DEPARTMENT}>SALE</h4>
-            <h4 className={styles.DEPARTMENT}>NEWS</h4>
-            <h4 className={styles.DEPARTMENT}>ABOUT</h4>
+        <div className={styles.loggerDepartments}>
+          <div className={styles.departments}>
+            <h4 className={styles.department}>BOOKS</h4>
+            <h4 className={styles.department}>SALE</h4>
+            <h4 className={styles.department}>NEWS</h4>
+            <h4 className={styles.department}>ABOUT</h4>
           </div>
-          <div className={styles.NAVBARLOGGERS}>
+          <div className={styles.navbarLoggers}>
             <h6>LOG IN</h6>
             <h6>SIGN UP</h6>
           </div>
         </div>
       </div>
-      <div hidden={mobileMenuIsHidden} className={styles.MOBILEMENU}>
-        <h4 className={styles.MOBILEDEPARTMENT}>BOOKS</h4>
-        <h4 className={styles.DEPARTMENT}>SALE</h4>
-        <h4 className={styles.DEPARTMENT}>NEWS</h4>
-        <h4 className={styles.DEPARTMENT}>ABOUT</h4>
+      <div hidden={mobileMenuIsHidden} className={styles.mobileMenu}>
+        <h4 className={styles.mobileDepartment}>BOOKS</h4>
+        <h4 className={styles.department}>SALE</h4>
+        <h4 className={styles.department}>NEWS</h4>
+        <h4 className={styles.department}>ABOUT</h4>
         <h6>LOG IN</h6>
         <h6>SIGN UP</h6>
       </div>
