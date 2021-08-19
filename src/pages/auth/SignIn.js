25fcef5d00e3940a1a10904984af6ee750784fb3
Edmon <you@example.com>
2021-08-19 13:11:08 +0400

    feat(handleSign): create handleSignIn

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -1,34 +1,39 @@
 import React from "react";
 import { useFormik } from "formik";
 
-import { signInSchema,signInInitValues } from "../../utils/schemas/signInSchema";
-import styles from './Auth.module.css'
+import {
+  signInSchema,
+  signInInitValues,
+} from "../../utils/schemas/signInSchema";
+import styles from "./Auth.module.css";
 
 const SignIn = () => {
-    const {handleSubmit,errors,touched,getFieldProps} = useFormik({
-      initialValues: signInInitValues,
-      validationSchema: signInSchema,
-      onSubmit: (values) => {
-        alert(JSON.stringify(values, null, 2));
-      },
-    });
-    return (
-      <form onSubmit={handleSubmit}>
-        
-        <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" {...getFieldProps("email")} />
-        {touched.email && errors.email ? (
-          <div className={styles.errors}>{errors.email}</div>
-        ) : null}
-  
-        <label htmlFor="password">Password</label>
-        <input id="password" type="text" {...getFieldProps("password")} />
-        {touched.password && errors.password ? (
-          <div className={styles.errors}>{errors.password}</div>
-        ) : null}
-  
-        <button type="submit">Submit</button>
-      </form>
-    );
+  const handleSign = (values) => {
+    console.log(values);
   };
-  export default SignIn;
+
+  const { handleSubmit, errors, touched, getFieldProps } = useFormik({
+    initialValues: signInInitValues,
+    validationSchema: signInSchema,
+    onSubmit: handleSign,
+  });
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input id="email" type="email" {...getFieldProps("email")} />
+      {touched.email && errors.email ? (
+        <div className={styles.errors}>{errors.email}</div>
+      ) : null}
+
+      <label htmlFor="password">Password</label>
+      <input id="password" type="text" {...getFieldProps("password")} />
+      {touched.password && errors.password ? (
+        <div className={styles.errors}>{errors.password}</div>
+      ) : null}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default SignIn;
