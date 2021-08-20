40befcfdede433a08c129f04ba8239b6ab55116e
Anna Harutyunyan <anuanharutyunyan>
2021-08-20 14:43:16 +0400

    Feat: Card example

--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -1,15 +1,20 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 
-import HomePage from '../pages/homepage/HomePage';
-import AdminPage from '../pages/adminpage/AdminPage';
+import HomePage from "../pages/homepage/HomePage";
+import AdminPage from "../pages/adminpage/AdminPage";
+import Cards from "../Cards/Cards";
+import IncDec from "../Cards/IncDec";
+
 
 const Routes = (props) => {
-    return (
-        <Switch>
-            <Route exact path='/admin' children={<AdminPage/>} />
-            <Route exact path='/' children={<HomePage/>} />
-        </Switch>
-    )
-}
+  return (
+    <Switch>
+      <Route exact path="/admin" children={<AdminPage />} />
+      <Route exact path="/" children={<HomePage />} />
+      <Route exact path="/cards" children={<Cards />} />
+      <Route exact path ='/inc'  children={<IncDec />} />
+    </Switch>
+  );
+};
 
 export default Routes;
