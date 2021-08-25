474ca0100d99c5cd2a5e66da2331d46a13b84464
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-08-25 19:09:36 +0400

    feat(NewsItem.jsx): add newsItem example and styles

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,30 +1,23 @@
-import styles from "./News.module.css";
 import img from "../about/images/azg.jpg";
+import "./News.css";
+import NewsItem from "./NewsItem";
 
 export default function News(props) {
   return (
-      <ul className={styles.news}>
-        <li className={styles.newsItem}>
-          <div className={styles.newsItemContent}>
-            <div  className={styles.newsItemPart}>
-              <span>25.08.2021</span>
-              <h3 className={styles.title}>Titleeee</h3>
-              <p className={styles.desc}>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Facilis, aut. Minima at temporibus eaque velit dicta
-                consequuntur aliquid facilis id voluptatem dignissimos, vel
-                explicabo ipsam, error doloremque fuga porro sapiente maxime
-                quos fugiat mollitia magnam tempora maiores, dolorum fugit?
-                Animi ipsa velit natus aut maiores, harum expedita quasi culpa
-                quod.
-              </p>
-            </div>
-            <div className={styles.newsItemPart}>
-              <img src={img}></img>
-            </div>
-          </div>
-          <hr></hr>
-        </li>
-      </ul>
+    <ul className="news">
+      {/* we will get this info from DB and map this component */}
+      <NewsItem
+        date="25.08.2021"
+        title="news Title"
+        desc="lorem ipsum ......."
+        imgSrc={img}
+      />
+      <NewsItem
+        date="25.08.2021"
+        title="news Title"
+        desc="lorem ipsum ......."
+        imgSrc={img}
+      />
+    </ul>
   );
 }
---
+++ src/pages/news/NewsItem.jsx
@@ -0,0 +1,20 @@
+
+export default function NewsItem({title, desc, date, imgSrc}) {
+    return(
+          <li className="newsItem">
+        <div className="newsItemContent">
+          <div className="newsItemPart">
+            <span>{date}</span>
+            <h3 className="newsTitle">{title}</h3>
+            <p className="newsDesc">
+             {desc}
+            </p>
+          </div>
+          <div className="newsItemPart">
+            <img width="500px" height="300px" src={imgSrc}></img>
+          </div>
+        </div>
+        <hr></hr>
+      </li> 
+    )
+}
