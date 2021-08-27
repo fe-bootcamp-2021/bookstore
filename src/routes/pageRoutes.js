eaf8bd2f6d18a519d516a2323420bee15b57e5f6
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-08-27 20:30:49 +0400

    feat(pageRoutes.js)Modify Childrens to Components in pageRoutes :

--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -11,30 +11,35 @@ import News from "../pages/news/News";
 import Cart from "../components/Cart/Cart";
 import AboutPage from "../pages/about/about";
 
-
 const Routes = (props) => {
   const currentUser = useSelector((state) => state.users.currentUser);
 
   return (
     <Switch>
-      <Route
-        exact
-        path="/admin"
-        children={
-          currentUser &&
-          currentUser.isAdmin &&
-          adminIds.includes(currentUser.localId) ? (
-            <AdminPage />
-          ) : (
-            () => <h3>You must be admin to view this page</h3>
-          )
-        }
-      />
-      <Route exact path="/" children={<HomePage />} />
-      <Route path="/auth" children={<AuthPage />} />
-      <Route path="/news" children={<News />} />
-      <Route path="/book/:id" children={<BookDetailPage />} />
-      <Route path="/about" children={<AboutPage />} />
+      <Route exact path="/admin">
+        {currentUser &&
+        currentUser.isAdmin &&
+        adminIds.includes(currentUser.localId) ? (
+          <AdminPage />
+        ) : (
+          () => <h3>You must be admin to view this page</h3>
+        )}
+      </Route>
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+      <Route path="/auth">
+        <AuthPage />
+      </Route>
+      <Route path="/news">
+        <News />
+      </Route>
+      <Route path="/book/:id">
+        <BookDetailPage />
+      </Route>
+      <Route path="/about">
+        <AboutPage />
+      </Route>
     </Switch>
   );
 };
