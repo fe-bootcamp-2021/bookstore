e0a959c28888efb552eb05990d0a78f2ec1c517c
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-08-31 17:17:45 +0400

    feat(News): add light/dark mode in news page

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,9 +1,21 @@
+import { useSelector } from "react-redux";
+import cn from "classnames";
+
 import "./News.css";
+
 import NewsItem from "./NewsItem";
 
 export default function News(props) {
+  const { isDark } = useSelector((state) => state);
+
   return (
-    <ul className="news">
+    <ul
+      className={cn(
+        "news",
+        { newsDarkMode: isDark },
+        { newsLightMode: !isDark }
+      )}
+    >
       {/* we will get this info from DB and map this component */}
       <NewsItem
         date="25.08.2021"
