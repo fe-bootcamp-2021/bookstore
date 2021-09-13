320737661579236155528c57ceabadcb56fb7c7d
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-09-13 15:53:10 +0400

    feat(news.jsx): get news from firebase

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,13 +1,20 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import cn from "classnames";
 
 import "./News.css";
 
 import NewsItem from "./NewsItem";
+import { getNews } from "../../redux/ducks/newsSlice";
 
 export default function News(props) {
+  const dispatch = useDispatch()
   const { isDark } = useSelector((state) => state);
+  const { news } = useSelector((state) => state);
 
+  useEffect(()=>{
+    dispatch(getNews())
+  },[])
+  console.log(news, ':::NEWSSSS');
   return (
     <ul
       className={cn(
